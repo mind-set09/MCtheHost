bd65694ed77c2f4ce10c6b5961d348a28eda8676
mind-set09 <141085830+mind-set09@users.noreply.github.com>
2023-08-15 17:55:10 -0400

    Update signup.php

--- backend/signup.php
+++ backend/signup.php
@@ -1,10 +1,5 @@
 <?php
-// Connect to your database
-$db_connection = mysqli_connect("localhost", "username", "password", "database_name");
-
-if (!$db_connection) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
+require_once 'db_connection.php'; // Include the database connection
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
